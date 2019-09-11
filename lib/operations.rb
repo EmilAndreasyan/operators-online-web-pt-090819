5cb6f73c2d461aec69bcc4516374d3d2e02b7ff4
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-09-11 03:02:32 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -6,7 +6,7 @@ if speed < 40 || speed > 60
 else puts "you're within the speed limits"
 end
 
-
+binding.pry
 
 def not_safe?(speed)
 	if speed < 40 || speed > 60 ? puts "Going 30 mph on the freeway would be unsafe, as would going 95 mph." : puts "you're within the speed limits"
